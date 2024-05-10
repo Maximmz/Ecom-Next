828c08974cdd9583bc8b74908429b68b32556294
Maximmz <108608264+Maximmz@users.noreply.github.com>
2024-05-10 23:47:59 +0500

    Additional designing for Category Design

--- src/app/admin/_components/CatDesign.tsx
+++ src/app/admin/_components/CatDesign.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { Button } from '@/components/ui/Button';
+import { X } from 'lucide-react';
 import React, { useState } from 'react';
 
 interface Product {
@@ -14,16 +15,22 @@ interface CatDesignProps {
 export default function CatDesign({ products }: CatDesignProps) {
     const [selected, setSelected] = useState<Product[]>([]);
     function handleClick(product: Product) {
-        setSelected([...selected, product]);
+        if (selected.includes(product)) {
+            // If the product is already selected, remove it
+            setSelected(selected.filter(item => item !== product));
+        } else {
+            // If the product is not selected, add it
+            setSelected([...selected, product]);
+        }
     }
     console.log(selected);
     return (
         <div>
-            <div>CatDesign</div>
             {/* Render buttons based on products */}
             {products.map(product => (
-                <Button key={product.id} onClick={() => handleClick(product)} className={selected.includes(product) ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'}>
+                <Button key={product.id} onClick={() => handleClick(product)} className={selected.includes(product) ? 'bg-slate-600' : 'bg-slate-400'}>
                     {product.name}
+                    {selected.includes(product) ? <span className="ps-2"><X size={16} strokeWidth={2.25} /></span> : <span></span>} 
                 </Button>
             ))}
         </div>
